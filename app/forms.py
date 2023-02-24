1b08965cb567b412bc0c2e86eae13fbf2f56d3a5
Jamila McGowan <jamcarr280@gmail.com>
2023-02-23 18:28:49 -0800

    Changes to import handlers etc..

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
-#from wtforms.validators import InputRequired, Email
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import InputRequired, Email
+#from wtforms.validators import DataRequired, Email
 
 
 
 class ContactForm(FlaskForm):
 
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[InputRequired()])
     #firstname = StringField('First Name', validators=[InputRequired()])
     #lastname = StringField('Last Name', validators=[InputRequired()])
-    email = StringField('Email', validators=[IDataRequired(), Email()])
-    subject = StringField('Subject', validators=[DataRequired()])
-    message = TextAreaField('Message', validators=[DataRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    subject = StringField('Subject', validators=[InputRequired()])
+    message = TextAreaField('Message', validators=[InputRequired()])
